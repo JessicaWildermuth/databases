b38bc3a7eb43e991d7966b3941fd00ca089debe0
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-09 23:42:48 -0700

    Create schema for database

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,36 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomName VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  content VARCHAR(120) NOT NULL,
+  user_id INT NOT NULL,
+  room_id INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(user_id) references users(id),
+  FOREIGN KEY(room_id) references rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE friends (
+  user_id INT NOT NULL,
+  friend_id INT NOT NULL,
+  FOREIGN KEY(user_id) references users(id),
+  FOREIGN KEY(friend_id) references users(id)
+);
 
 
 
