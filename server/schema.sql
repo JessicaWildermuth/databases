fd2ea2a9d5dfb0beb70de103906e0a0fe0ed8383
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-10 15:05:35 -0700

    Update spec to be less complex for MVP

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,11 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE rooms (
-  id INT NOT NULL AUTO_INCREMENT,
-  roomName VARCHAR(20),
-  PRIMARY KEY(id)
-);
+-- CREATE TABLE rooms (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   roomName VARCHAR(20),
+--   PRIMARY KEY(id)
+-- );
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
@@ -17,21 +17,21 @@ CREATE TABLE users (
 CREATE TABLE messages (
   id INT NOT NULL AUTO_INCREMENT,
   content VARCHAR(120) NOT NULL,
-  user_id INT NOT NULL,
-  room_id INT NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(user_id) references users(id),
-  FOREIGN KEY(room_id) references rooms(id)
+  -- user_id INT NOT NULL,
+  -- room_id INT NOT NULL,
+  PRIMARY KEY(id)
+  -- FOREIGN KEY(user_id) references users(id),
+  -- FOREIGN KEY(room_id) references rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
 
-CREATE TABLE friends (
-  user_id INT NOT NULL,
-  friend_id INT NOT NULL,
-  FOREIGN KEY(user_id) references users(id),
-  FOREIGN KEY(friend_id) references users(id)
-);
+-- CREATE TABLE friends (
+--   user_id INT NOT NULL,
+--   friend_id INT NOT NULL,
+--   FOREIGN KEY(user_id) references users(id),
+--   FOREIGN KEY(friend_id) references users(id)
+-- );
 
 
 
