d88a381361513033d3f60e6d2ce55c927fc666ab
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-09 23:44:59 -0700

    Begin get function creation

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,9 @@
 var models = require('../models');
-
+//models.messages.getAll
+// console.log(models.messages.getAll);
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
+  get: function (req, res) {
+    res.writeHead(102, headers);
+  }, // a function which handles a get request for all messages
   post: function (req, res) {} // a function which handles posting a message to the database
 };
