00e2f3475ef578895be89847dd37ae502d1cca40
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-09 23:44:37 -0700

    Write getAll function

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,20 @@
 var db = require('../db');
 
+
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
-  create: function () {} // a function which can be used to insert a message into the database
+  getAll: function (callback) {
+    db.query('select * from messages', (err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  // a function which produces all the messages
+  create: function (message, callback) {
+    // db.query('INSERT INTO messages (message, user) values ()');
+
+
+  } // a function which can be used to insert a message into the database
 };
